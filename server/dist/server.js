e8a9179a07ed8f16cb683c279956a764fcc7cb49
qianchen <qianchen@douyu.tv>
2023-04-07 19:47:18 +0800

    feat: docker及nginx初始化配置

---
+++ server/dist/server.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get("/", (req, res, next) => {
+    console.log("req /");
+    res.send("11111");
+});
+const server = app.listen(3000, () => {
+    console.log("listen port 3000");
+});
+exports.default = server;
+//# sourceMappingURL=server.js.map
---
+++ server/src/server.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import { Response, Request, NextFunction } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+    console.log("req /")
+    res.send("11111");
+})
+
+const server = app.listen(3000, () => {
+    console.log("listen port 3000")
+})
+
+export default server;
