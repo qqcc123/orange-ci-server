833370f5f9c61675c344380d94b50ee8e6fe764c
qianchen <qianchen@douyu.tv>
2023-04-10 14:20:43 +0800

    feat: 添加body-parser 模块

--- server/dist/server.js
+++ server/dist/server.js
@@ -4,10 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
+app.use(body_parser_1.default.urlencoded({ extended: false }));
 app.get("/", (req, res, next) => {
     console.log("req /");
     res.send("11111");
+});
+app.post("/", (req, res, next) => {
+    console.log("post /");
+    res.send("22222");
 });
 const server = app.listen(3000, () => {
     console.log("listen port 3000");
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,14 @@
 import express from "express"
 import { Response, Request, NextFunction } from "express";
+import bodyParser from "body-parser"
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false }))
+
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
     console.log("req /")
-    res.send("11111");
+    res.send("11111 new");
 })
 
 app.post("/", (req: Request, res: Response, next: NextFunction) => {
